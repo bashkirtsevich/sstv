a785fc0e2b49f79705e8d5d4d539e18ac362a0f4
bashkirtsevich <bashkirtsevich@gmail.com>
2025-03-27 02:10:26 +0500

    Fix MMSSTVMCNMeta LINE_TIME

--- modes/mmsstv/mcn.py
+++ modes/mmsstv/mcn.py
@@ -9,7 +9,7 @@ class MMSSTVMCNMeta(ModeMetaclass):
         cls.CHAN_OFFSETS.append(cls.CHAN_OFFSETS[0] + cls.CHAN_TIME)
         cls.CHAN_OFFSETS.append(cls.CHAN_OFFSETS[1] + cls.CHAN_TIME)
 
-        cls.LINE_TIME = cls.SYNC_PULSE + cls.SYNC_PORCH * 5.8 + cls.CHAN_COUNT * cls.CHAN_TIME
+        cls.LINE_TIME = cls.CHAN_OFFSETS[2] + cls.SCAN_TIME
         cls.PIXEL_TIME = cls.SCAN_TIME / cls.LINE_WIDTH
 
         cls.TIMING_SEQUENCE = [
